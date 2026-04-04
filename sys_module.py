e78b5976fbe767cf799603d0693ef610854b0eab
Gautam Regmi <gautam.regmi.bigdata@gmail.com>
2026-04-05 03:59:54 +0530

    connected python with sql

--- sys_module.py
+++ sys_module.py
@@ -1,5 +1,5 @@
-import sys
-from pathlib import Path
+# import sys
+# from pathlib import Path
 
 # # readme_file=Path('README.md')
 # # with open(readme_file,'r')as rm:
@@ -41,5 +41,31 @@ from pathlib import Path
 # # Display Hook
 
 
-print(sys.platform)
-print(sys.version)
+# print(sys.platform)
+# print(sys.version)
+
+# 
+
+l=[1,2,3,4,5,6,7,8,9]
+k=[1,2,3,4,5,6,7,8,0]
+
+for i in range(0,len(l)-1,2):
+    k[i],k[i+1]=k[i+1],k[i]
+print(k)
+
+a=l.copy()
+i=0
+while i+1 < len(a):
+    a[i],a[i+1]=a[i+1],a[i]
+    i+=2
+print(a)
+
+from sqlalchemy import create_engine
+
+engine=create_engine("mysql+pymysql://root:1234@127.0.0.1:3306/madrid")
+import pandas as pd
+df=pd.read_sql("select * from age_group_distribution",engine)
+# print(df.head())
+
+import sys
+print(sys.executable)
